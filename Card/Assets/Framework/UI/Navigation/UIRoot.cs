d3293a521499fa2d69c263ce2538f520470de3e7
alongsama <543456693@qq.com>
2026-09-08 11:09:06 +0800

    增加宽屏适配

--- Card/Assets/Framework/UI/Navigation/UIRoot.cs
+++ Card/Assets/Framework/UI/Navigation/UIRoot.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Framework.UI.Navigation
 {
@@ -20,6 +21,12 @@ namespace Framework.UI.Navigation
         [SerializeField] private RectTransform _resourceLayer;
         [SerializeField] private RectTransform _guideLayer;
         [SerializeField] private RectTransform _toastLayer;
+        [SerializeField] private RectTransform _blackLeft;
+        [SerializeField] private RectTransform _blackRight;
+
+        private CanvasScaler _scaler;
+        private int _lastWidth;
+        private int _lastHeight;
 
         public Canvas RootCanvas { get; private set; }
 
@@ -63,6 +70,117 @@ namespace Framework.UI.Navigation
         private void Awake()
         {
             EnsureInitialized();
+        }
+
+        private void OnEnable()
+        {
+            ApplyWidescreenFit(true);
+        }
+
+        private void LateUpdate()
+        {
+            ApplyWidescreenFit(false);
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            ApplyWidescreenFit(true);
+        }
+#endif
+
+        /// <summary>
+        /// 宽于 9:16 时按高度适配（与 GameHud 锁定设计高度一致），左右用黑边遮挡；
+        /// 否则保持按宽度适配，黑边宽度为 0。
+        /// </summary>
+        private void ApplyWidescreenFit(bool force)
+        {
+            int width = Screen.width;
+            int height = Screen.height;
+            if (width <= 0 || height <= 0)
+            {
+                return;
+            }
+
+            EnsureInitialized();
+
+            if (!force && width == _lastWidth && height == _lastHeight)
+            {
+                return;
+            }
+
+            _lastWidth = width;
+            _lastHeight = height;
+
+            if (_scaler == null)
+            {
+                _scaler = GetComponent<CanvasScaler>();
+            }
+
+            Vector2 reference = _scaler != null
+                ? _scaler.referenceResolution
+                : new Vector2(1080f, 1920f);
+            float designWidth = reference.x;
+            float designHeight = reference.y;
+            if (designWidth <= 0f || designHeight <= 0f)
+            {
+                return;
+            }
+
+            float designAspect = designWidth / designHeight;
+            float currentAspect = (float)width / height;
+            float barWidth = 0f;
+            if (currentAspect > designAspect)
+            {
+                if (_scaler != null)
+                {
+                    _scaler.matchWidthOrHeight = 1f;
+                }
+
+                barWidth = (designHeight * currentAspect - designWidth) * 0.5f;
+            }
+            else if (_scaler != null)
+            {
+                _scaler.matchWidthOrHeight = 0f;
+            }
+
+            SetBarWidth(_blackLeft, barWidth);
+            SetBarWidth(_blackRight, barWidth);
+            InsetLayers(barWidth);
+        }
+
+        private static void SetBarWidth(RectTransform bar, float width)
+        {
+            if (bar == null)
+            {
+                return;
+            }
+
+            var size = bar.sizeDelta;
+            size.x = width;
+            bar.sizeDelta = size;
+        }
+
+        private void InsetLayers(float barWidth)
+        {
+            float sizeX = -barWidth * 2f;
+            foreach (var pair in _layers)
+            {
+                var rect = pair.Value;
+                if (rect == null)
+                {
+                    continue;
+                }
+
+                var size = rect.sizeDelta;
+                size.x = sizeX;
+                rect.sizeDelta = size;
+            }
         }
 
         private void EnsureInitialized()

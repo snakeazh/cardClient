bfe37186f3920e4c9cf26a2f3df1846c5a97dbb6
snake_www <18530895881@163.com>
2026-09-23 14:44:47 +0800

    xx

--- Card/Assets/App/Net/GameApiSettings.cs
+++ Card/Assets/App/Net/GameApiSettings.cs
@@ -5,8 +5,8 @@ namespace App.Net
     /// <summary>局外 HTTP 短链地址。Editor / 本机默认连 Development 服务。</summary>
     public static class GameApiSettings
     {
-        public const string DefaultBaseUrl = "http://121.43.167.98:5254";
-
+        // public const string DefaultBaseUrl = "http://121.43.167.98:5254";
+        public const string DefaultBaseUrl = "http://127.0.0.1:5254";
         public static string BaseUrl { get; set; } = DefaultBaseUrl;
 
         public static string WebSocketUrl
